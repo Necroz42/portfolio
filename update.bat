7458fd2154726b2d588a08d8d402da46e407b99b
BONIN Ethan <ebonin@et.esiea.fr>
2025-05-26 09:43:22 +0200

    script update

---
+++ update.bat
@@ -0,0 +1,15 @@
+@echo off
+echo 📦 Sauvegarde des fichiers modifiés...
+git add .
+
+set /p msg=📝 Entrez un message de commit :
+git commit -m "%msg%"
+
+echo ⬆️  Push vers la branche main...
+git push origin main
+
+echo 🚀 Déploiement sur GitHub Pages...
+npm run deploy
+
+echo ✅ Portfolio mis à jour en ligne !
+pause
